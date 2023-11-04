f21e1b6c778b21cc3f5728864d772b0239b1b5d0
Humair Shoukat <88780542+humairshoukat@users.noreply.github.com>
2023-11-04 19:53:11 +0500

    updated personal details

--- src/App.jsx
+++ src/App.jsx
@@ -24,15 +24,15 @@ import "./styles.css";
  * If you don't have one of the social sites listed, leave it as an empty string.
  */
 const siteProps = {
-  name: "Alexandrie Grenier",
-  title: "Web Designer & Content Creator",
-  email: "alex@example.com",
-  gitHub: "microsoft",
-  instagram: "microsoft",
-  linkedIn: "satyanadella",
-  medium: "",
-  twitter: "microsoft",
-  youTube: "Code",
+  name: "Humair Shoukat",
+  title: "Full Stack Developer",
+  email: "humair.shoukat@studentambassadors.com",
+  gitHub: "humairshoukat",
+  instagram: "humairshoukat",
+  linkedIn: "humairshoukat",
+  medium: "humairshoukat",
+  twitter: "humairshoukat",
+  youTube: "@humairshoukat",
 };
 
 const primaryColor = "#4E567E";
